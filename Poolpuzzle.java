834bf0596987ae7655cfaf856a3438dd30157709
sandeepkumar28 <46688693+sandeepkumar28@users.noreply.github.com>
2019-05-16 00:56:06 +0530

    Add files via upload

---
+++ Poolpuzzle.java
@@ -0,0 +1,27 @@
+class Poolpuzzle{
+public static void main(String[] args)
+{
+int x=0;
+while(x<1){
+System.out.print("a");
+if(x<1){
+System.out.print(" ");
+}
+System.out.print("noise");
+if(x<4)
+{
+System.out.print("annoys");
+x=x+1;
+}
+if(x==1){
+x=x+2;
+}
+if(x>1)
+{
+System.out.print("an");
+}
+System.out.print(" oyster");
+x=x-2;	
+}
+}
+}
